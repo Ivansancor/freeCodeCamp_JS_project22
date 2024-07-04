dc66966b2f41d844f225d4c148c9d77ffbe8ff9a
Ivansancor <ivanvaldeolivo95@gmail.com>
2024-07-04 02:17:38 +0200

    added extra DOM elements for clarity while interacting with app

--- script.js
+++ script.js
@@ -1,14 +1,107 @@
+const priceOfPurchase = document.getElementById("price-of-purchase");
 const cashInput = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
-const changeDue = document.getElementById("change-due");
+const changeDueDiv = document.getElementById("change-due");
+const registerBoxDrawerList = document.getElementById("register-box-drawer-list");
+const registerBoxScreenTotal = document.getElementById("register-box-screen-total");
 const cashReceivedP = document.getElementById("cash-received-p")
 const cashReceivedSpan = document.getElementById("cash-received-span")
+const changeDueP = document.getElementById("change-due-p")
+const changeDueSpan = document.getElementById("change-due-span")
 
 
 const priceToPay = 3.26;
 
+const change = [
+    {name: "Pennies",
+        quantity: 101,
+        value: 0.01
+    },
+    {name: "Nickels",
+        quantity: 41,
+        value: 0.05
+    },
+    {name: "Dimes",
+        quantity: 31,
+        value: 0.1
+    },
+    {name: "Quarters",
+        quantity: 17,
+        value: 0.25
+    },
+    {name: "Ones",
+        quantity: 90,
+        value: 1
+    },
+    {name: "Fives",
+        quantity: 11,
+        value: 5
+    },
+    {name: "Tens",
+        quantity: 2,
+        value: 10
+    },
+    {name: "Twenties",
+        quantity: 3,
+        value: 20
+    },
+    {name: "Hundreds",
+        quantity: 1,
+        value: 100
+    }
+]
 
-const calculateChange = () => {
+const calculateBoxTotalValue = () => {
+    return (change.reduce((acc, item) => acc + item.quantity * item.value, 0));
+}
+
+const displayPricetoPay = () => {
+    priceOfPurchase.textContent = `Price to pay: $${priceToPay.toFixed(2)}`;
+}
+
+const displayBoxContents = () => {
+    let htmlInjection = "";
+    for(item of change) {
+        htmlInjection += `<li>${item.name}: $${(item.value*item.quantity).toFixed(2)}<li>`;
+    }
+    registerBoxDrawerList.innerHTML = htmlInjection;
+    registerBoxScreenTotal.textContent = `Total change available: $${calculateBoxTotalValue().toFixed(2)}`
+}
+
+window.onload = () => {
+    displayPricetoPay();
+    displayBoxContents();
+}
+// const calculateChange = cashToEval => {
+//     const totalValueInBox = Number(calculateBoxTotalValue(change));
+//     const statusTxt = (cashToEval - priceToPay) > totalValueInBox ? "INSUFFICIENT_FUNDS" : (cashToEval - priceToPay) === totalValueInBox ? "CLOSED" : "OPEN";
+
+//     changeDue.innerHTML = `
+//         <span>Status: ${statusTxt}</span>
+//         <p></p>
+//         `;
+//         changeDue.style.display = "block";
+
+    
+// }
+
+const displayStats = (paid, due) => {
+    cashReceivedSpan.innerHTML= `${paid.toFixed(2)}`;
+    cashReceivedP.style.display = "block";
+    changeDueSpan.textContent = `${due.toFixed(2)}`;
+    changeDueP.style.display = "block";
+}
+const displayChangeDue = due => {
+    if (due === 0) {
+        changeDueDiv.textContent = "No change due - customer paid with exact cash";
+        changeDueDiv.style.display = "block";
+    }
+    else {
+        changeDueDiv.textContent = "You owe some green back to customer!";
+        changeDueDiv.style.display = "block";
+    }
+}
+const evaluateInput = () => {
     if (cashInput.value === "") {
         alert("Please enter an amount");
         return;
@@ -17,63 +110,35 @@ const calculateChange = () => {
         const regex = /e|\+|-/gi;
         const cashReceived = Number(cashInput.value.replace(regex, ""));
         cashInput.value = "";
-
+        const changeDue = (cashReceived - priceToPay);
+        console.log(cashReceived)
+        console.log(priceToPay)
+        console.log(changeDue)
+        console.log(typeof(cashReceived))
+        console.log(typeof(priceToPay))
+        console.log(typeof(changeDue))
+        
         if (cashReceived < priceToPay) {
             alert("Customer does not have enough money to purchase the item");
             return;
-        } else if (cashReceived === priceToPay) {
-            changeDue.textContent = "No change due - customer paid with exact cash";
-            changeDue.style.display = "block";
-            cashReceivedSpan.innerHTML= `${cashReceived} &#8680;`;
-            cashReceivedP.style.display = "block"
+        } else if (cashReceived === Number(priceToPay)) {
+            displayStats(cashReceived, changeDue);
+            displayChangeDue(0);
+            return;
         } else {
+            displayStats(cashReceived, changeDue);
+            // calculateChange(cashReceived);
+            displayChangeDue(1);
         }
-        
-        // const status = cashReceived === priceToPay ? "CLOSED" : cashReceived > priceToPay ? "OPEN" : "INSUFFICIENT_FUNDS";
-        // changeDue.innerHTML = `
-        // <span>Status: ${status}</span>
-        // <p></p>
-        // `;
-        // changeDue.style.display = "block";
 
     }
 }
 
 
-// const change = [
-//     {name: pennies,
-//         quantity: 101
-//     },
-//     {name: nickels,
-//         quantity: 21
-//     },
-//     {name: dimes,
-//         quantity: 31
-//     },
-//     {name: quarters,
-//         quantity: 17
-//     },
-//     {name: ones,
-//         quantity: 90
-//     },
-//     {name: fives,
-//         quantity: 11
-//     },
-//     {name: tens,
-//         quantity: 2
-//     },
-//     {name: twenties,
-//         quantity: 3
-//     },
-//     {name: hundreds,
-//         quantity: 1
-//     }
-// ]
-
 cashInput.addEventListener("keyup", e => {
  if (e.key === "Enter") {
-    calculateChange();
+    evaluateInput();
 }
 });
 
-purchaseBtn.addEventListener("click", calculateChange);
+purchaseBtn.addEventListener("click", evaluateInput);
